bdc5f34dd8db227b866ef8d9f59ae1872db43121
Mikhail Zhdanov <46405630+mihail-jdanov@users.noreply.github.com>
2026-05-06 16:11:03 +0700

    Update PhoneNumberTextField.swift

--- PhoneNumberKit/UI/PhoneNumberTextField.swift
+++ PhoneNumberKit/UI/PhoneNumberTextField.swift
@@ -34,10 +34,10 @@ open class PhoneNumberTextField: UITextField, UITextFieldDelegate {
         }
     }
     /// Forces textContentType to .telephoneNumber for autofill support.
-    override open var textContentType: UITextContentType! {
-        get { .telephoneNumber }
-        set {}
-    }
+    // override open var textContentType: UITextContentType! {
+    //     get { .telephoneNumber }
+    //     set {}
+    // }
 
     /// allows text to be set without formatting
     open func setTextUnformatted(newValue: String?) {
